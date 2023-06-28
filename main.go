6a0647c51cc12d8aa516623444364c905412d920
moryku <mohammadrizkyk@gmail.com>
2023-06-28 19:42:10 +0700

    remove load env

--- main.go
+++ main.go
@@ -4,24 +4,23 @@ import (
 	"prakerja5/configs"
 	"prakerja5/routes"
 
-	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	loadEnv()
+	// loadEnv()
 	configs.InitDB() 
 	e := echo.New()
 	e = routes.InitRoute(e)
 	e.Start(":8000")
 }
 
-func loadEnv(){
-	err := godotenv.Load()
-  	if err != nil {
-    	panic("Failed load file env")
-  	}
-}
+// func loadEnv(){
+// 	err := godotenv.Load()
+//   	if err != nil {
+//     	panic("Failed load file env")
+//   	}
+// }
 
 
 
